fcadd4e1337a50cf686b9e8fd88ed549360b80a1
hanminsoo <hanminsoo@hanminsooui-MacBook-Pro.local>
2016-05-26 22:06:11 +0900

    update : function_test >> refectoring unittest

--- function_test.py
+++ function_test.py
@@ -1,39 +1,46 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
 
-# 민수(minsoo)는 멋진 작업목록 온라인 앱이 나아왔다는 소식을 듣고
-# 해당 웹사이트를 확인하러 가게 된다
-browser.get('http://localhost:8000')
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
+    def tearDown(self):
+        self.browser.quit()
 
-#웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다
-assert 'To-Do' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 민수는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
+        # 해당 웹사이트로 이동한다
+        self.browser.get('http://localhost:8000')
 
+        # 웹페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
-#그녀는 바로 작업을 추가하기로 한다
+        # 그는 바로 작업을 추가하기로 한다
 
-
-#"공작깃털 사기"라고 텍스트 상자에 입력한다
-#(민수의 취미는 날치 잡이용 그물을 만들어 내는 것이다)
-
-
-#엔터키를 치면 페이지가 갱신되고 작업목록에
-#"1:공작깃털 사기" 아이템이 추가된다
-
-
-#추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
-#다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다(민수는 매우 체계적인 사람이다)
+        #"공작깃털 사기"라고 텍스트 상자에 입력한다
+        #(민수의 취미는 날치 잡이용 그물을 만들어 내는 것이다)
 
 
-#페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다
-#민수는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
-#사이트는 그를 위한 특정 URL을 생성해준다
-#이때 URL에 대한 설명도 함께 제공한다.
+        #엔터키를 치면 페이지가 갱신되고 작업목록에
+        #"1:공작깃털 사기" 아이템이 추가된다
 
 
-#해당 URL에 접속하면 그가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다
+        #추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
+        #다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다(민수는 매우 체계적인 사람이다)
 
-#만족하고 잔다
 
-browser.quit()
+        #페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다
+        #민수는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
+        #사이트는 그를 위한 특정 URL을 생성해준다
+        #이때 URL에 대한 설명도 함께 제공한다.
+
+
+        #해당 URL에 접속하면 그가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다
+
+        #만족하고 잔다
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
