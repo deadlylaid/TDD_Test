6c3e1f141a6c751a96eafa5412616f8c66fda6a9
hanminsoo <hanminsoo@hanminsooui-MacBook-Pro.local>
2016-05-31 17:53:31 +0900

    update : code refector >> 상수 테스트를 제외함

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-from django.templates.loader import render_to_string
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -14,9 +14,10 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
         self.assertIn(b'<title>To-Do lists</title>', response.content)
 
 #strip() 은 텍스트 편집기에 따라 파일 마지막에 라인(\n)이 강제로 추가되는 것을 방지하기 위해 쓴다
-        self.assertTrue(response.content.strip().endswith(b'</html>'))
+#        self.assertTrue(response.content.strip().endswith(b'</html>'))
 # Create your tests here.
