1ceb3a1c0b2af2e07385767d76f40fffdd4eab1e
hanminsoo <hanminsoo@hanminsooui-MacBook-Pro.local>
2016-05-31 18:37:39 +0900

    update : create MainPage Html used templates

--- function_test.py
+++ function_test.py
@@ -34,10 +34,10 @@ class NewVisitorTest(unittest.TestCase):
 
         #엔터키를 치면 페이지가 갱신되고 작업목록에
         #"1:공작깃털 사기" 아이템이 추가된다
-        inputbox.send_keys(keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
                 any(row.text == '1: 공작깃털 사기' for row in rows),
         )
