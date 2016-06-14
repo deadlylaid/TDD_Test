98dfb42ae0ef40684ac90b57bcf3c06d0f41ef3b
hanminsoo <hanminsoo@hanminsooui-MacBook-Pro.local>
2016-06-14 19:09:56 +0900

    update : create URL and View for add New item in list

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -11,14 +11,15 @@ def home_page(request):
             'home.html',
             )
 
-def view_list(request):
+def view_list(request, list_id):
 
-    items = Item.objects.all()
+    list_ = List.objects.get(id=list_id)
+#    items = Item.objects.filter(list=list_)
 
     return render(
             request,
             'list.html',
-            {'items':items}
+            {'list':list_}
             )
 
 def new_list(request):
@@ -27,5 +28,10 @@ def new_list(request):
     Item.objects.create(text=request.POST['item_text'], list=list_)
    
     return redirect(
-            '/lists/the-only-list-in-the-world/'
+            '/lists/%d/' % (list_.id,)
             )
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' %(list_.id,))
