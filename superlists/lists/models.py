3d34f8d57c4b676231944f29b991569711e8386d
hanminsoo <hanminsoo@hanminsooui-MacBook-Pro.local>
2016-06-14 14:12:30 +0900

    update : create ForeignKey List-Item models

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -3,5 +3,16 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
 
-    text = models.TextField(default = '')
+    text = models.TextField(
+            default = ''
+            )
+
+    list = models.ForeignKey(
+            "List",
+            default=None
+            )
+
+
+class List(models.Model):
+    pass
 
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 def home_page(request): 
@@ -22,8 +22,9 @@ def view_list(request):
             )
 
 def new_list(request):
+    list_ = List.objects.create()
 
-    Item.objects.create(text=request.POST['item_text'])
+    Item.objects.create(text=request.POST['item_text'], list=list_)
    
     return redirect(
             '/lists/the-only-list-in-the-world/'
