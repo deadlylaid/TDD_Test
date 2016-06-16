3ef5107a5ccd6e4cd4e26b3203579e5ca83611ba
hanminsoo <hanminsoo@hanminsooui-MacBook-Pro.local>
2016-06-16 20:30:34 +0900

    update : The first step about layout and styling

--- superlists/functional_tests/test.py
+++ superlists/functional_tests/test.py
@@ -87,3 +87,26 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('공작깃털 사기', page_text)
         self.assertNotIn('그물 만들기', page_text)
+
+    def test_layout_and_styling(self):
+        #민수는 메인 페이지를 방문한다.
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        #그는 입력 상자가 가운데 배치된 것을 본다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=10
+                )
+
+        #그는 새로운 리스트를 시작하고
+        #입력상자가 가운데 배치되는 것을 확인한다.
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=10
+                )
