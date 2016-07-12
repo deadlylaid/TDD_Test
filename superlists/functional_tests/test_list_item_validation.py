37157fa249bf9d927d05ccac1a1627fdee087ad9
deadlylaid <deadlylaid@gmail.com>
2016-07-12 17:05:15 +0900

    update : modify functional_tests/test.py >> base.py, test_layout_and_styling.py test_list_item_validation.py, test_simple_list_creation.py

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -1,33 +1,13 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from unittest import skip
+
+from .base import FunctionalTest
 
 import sys
 
-class NewVisitorTest(StaticLiveServerTestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        for arg in sys.argv:
-            if 'liveserver' in arg:
-                cls.server_url = 'http://' + arg.split('=')[1]
-                return
-        super().setUpClass()
-        cls.server_url = cls.live_server_url
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        #rows 안에 있는 값들을 뽑아서 row.text라는 속성을 뽑은 후에 list에 담는다
-        #그 안에 row_text가 존재하는지 확인한다.
-        self.assertIn(row_text, [row.text for row in rows])
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 민수는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
@@ -98,26 +78,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('공작깃털 사기', page_text)
         self.assertNotIn('그물 만들기', page_text)
-
-    def test_layout_and_styling(self):
-        #민수는 메인 페이지를 방문한다.
-        self.browser.get(self.server_url)
-        self.browser.set_window_size(1024, 768)
-
-        #그는 입력 상자가 가운데 배치된 것을 본다
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-                inputbox.location['x'] + inputbox.size['width'] / 2,
-                512,
-                delta=10
-                )
-
-        #그는 새로운 리스트를 시작하고
-        #입력상자가 가운데 배치되는 것을 확인한다.
-        inputbox.send_keys('testing\n')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-                inputbox.location['x'] + inputbox.size['width'] / 2,
-                512,
-                delta=10
-                )
---
+++ superlists/functional_tests/test_simple_list_creation.py
@@ -0,0 +1,30 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from unittest import skip
+
+from .base import FunctionalTest
+
+import sys
+
+
+class ItemValidationTest(FunctionalTest):
+
+    def test_cannot_add_empty_list_item(self):
+        #에디스는 메인 페이지에 접속해서 빈 아이템을 실수로 등록하려고 한다
+        # 입력상자가 비어있는 상태에서 엔터키를 누른다
+
+        #페이지가 새로고침 되고, 빈아이템을 등록할 수 없다는
+        #메시지가 표시된다.
+
+        #다른 아이템을 입력하면 정상처리된다
+
+        #고의로 다시한번 빈 아이템을 등록한다
+
+        #리스트 페이지에 에러메시지가 표시된다.
+
+        #아이템을 입력하면 정상작동한다.
+
+        self.fail('wirte me!')
+
+
